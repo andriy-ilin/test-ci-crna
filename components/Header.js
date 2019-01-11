a1d8a227ec2a251e022c6b561e10ce3a7ca889ad
andriy-ilin <andriy.ilin90@gmail.com>
2019-01-11 19:59:54 +0200

    feat: use custom Header on article screen

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,17 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Platform, I18nManager, StyleSheet } from "react-native";
+
+const APPBAR_HEIGHT = Platform.OS === "ios" ? 44 : 56;
+const STATUSBAR_HEIGHT = Platform.OS === "ios" ? 20 : 0;
+
+const TITLE_OFFSET_CENTER_ALIGN = Platform.OS === "ios" ? 70 : 56;
+const TITLE_OFFSET_LEFT_ALIGN = Platform.OS === "ios" ? 20 : 56;
 
 export default class Header extends React.Component {
+  static get HEIGHT() {
+    return APPBAR_HEIGHT + STATUSBAR_HEIGHT;
+  }
+
   render() {
     return (
       <View>
@@ -10,3 +20,7 @@ export default class Header extends React.Component {
     );
   }
 }
+
+const getAppBarHeight = () => {
+  return Platform.OS === "ios" ? 44 : 56;
+};
--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import { View, ScrollView, Text, Image } from "react-native";
 
+import Header from "../components/Header";
+
 export class ArticleScreen extends Component {
+  static navigationOptions = {
+    header: <Header />
+  };
   render() {
     return (
       <View>
