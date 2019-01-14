35592a2d171669314c5de5d2e64ced749ed2a714
andriy-ilin <andriy.ilin90@gmail.com>
2019-01-14 22:23:30 +0200

    feat: add LogoGradient and RightArrow icon for menu screen

--- icons/Logo.js
+++ icons/Logo.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Image } from "react-native";
 
-const Logo = props => (
-  <Image
-    {...props}
-    source={require("../assets/images/logoBlack.png")}
-    style={{ width: 84, height: 24 }}
-    resizeMode="cover"
-  />
-);
+const Logo = ({ gradient = false, ...props }) =>
+  gradient ? (
+    <Image
+      source={require("../assets/images/logoGradient.png")}
+      style={{ width: 158, height: 42 }}
+      resizeMode="cover"
+      {...props}
+    />
+  ) : (
+    <Image
+      source={require("../assets/images/logoBlack.png")}
+      style={{ width: 84, height: 24 }}
+      resizeMode="cover"
+      {...props}
+    />
+  );
 
 export default Logo;
