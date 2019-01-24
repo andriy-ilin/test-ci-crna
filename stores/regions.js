480d010765d50fec218e4b2b1b9b2f9c0abff464
andriy-ilin <andriy.ilin90@gmail.com>
2019-01-24 23:54:00 +0200

    feat: add RegionArticlesScreen to project

--- stores/regions.js
+++ stores/regions.js
@@ -1,4 +1,4 @@
-import { observable, computed, action } from "mobx";
+import { observable, computed, action, toJS } from "mobx";
 import api from "../services/api";
 import { entitiesFromFB } from "./utils";
 
@@ -10,9 +10,16 @@ class RegionsStore extends BasicStore {
 
   @observable entities = {};
   @observable regions = {};
+  @observable filterArticles = "";
+
+  @action regionArticles(filterArticles) {
+    return Object.values(toJS(this.entities)).filter(
+      ({ region }) => region === filterArticles
+    );
+  }
 
   @computed get listEntries() {
-    return Object.values(this.entities);
+    return Object.values(toJS(this.entities));
   }
 
   @computed get listRegions() {
@@ -28,8 +35,10 @@ class RegionsStore extends BasicStore {
 
   @action
   async getArticle(refName) {
+    this.set("loading", true);
     const data = await api.fetchAllByEntityName(refName);
     this.set("entities", data);
+    this.set("loading", false);
   }
 
   @action
