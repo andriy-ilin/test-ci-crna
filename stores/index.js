3a78bae3e99bcdd79a12e168fb1d053bccd50502
andriy-ilin <andriy.ilin90@gmail.com>
2019-01-25 00:02:09 +0200

    feat: use Switch with Text, Slider, Authors, Video in ArticleScreen

--- stores/index.js
+++ stores/index.js
@@ -2,12 +2,16 @@ import "../helpers/firebaseConfig";
 import Navigation from "./navigation";
 import Article from "./article";
 import Regions from "./regions";
+import Team from "./team";
+import Video from "./video";
 import Localization from "./localization";
 
 const stores = {};
 stores.navigation = new Navigation(stores);
 stores.article = new Article(stores);
 stores.regions = new Regions(stores);
+stores.team = new Team(stores);
+stores.video = new Video(stores);
 stores.localization = new Localization(stores);
 
 export default stores;
