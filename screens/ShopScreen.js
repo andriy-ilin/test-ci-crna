8f6cd470ca2bac49e8faed41fe333785d3291991
andriy-ilin <andriy.ilin90@gmail.com>
2019-01-27 23:05:40 +0200

    feat: add product list to shop screen

--- screens/ShopScreen.js
+++ screens/ShopScreen.js
@@ -1,22 +1,132 @@
 import React, { Component } from "react";
-import { observer } from "mobx-react/native";
+import { observer, inject } from "mobx-react/native";
 import { withNamespaces } from "react-i18next";
-import { View, ScrollView, Text, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  ScrollView,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList
+} from "react-native";
 import Title from "../components/Title";
+import Foto from "../components/Foto";
 import StyledText from "../components/StyledText";
+import Cart from "../icons/Cart";
 
 @withNamespaces(["shop"], { wait: true })
+@inject("shop")
 @observer
 export class ShopScreen extends Component {
+  async componentDidMount() {
+    const { shop } = this.props;
+    await shop.getProducts("/catalogShopItems/uk");
+  }
   render() {
-    const { t } = this.props;
+    const {
+      t,
+      navigation: { navigate },
+      shop: { list = [] } = {}
+    } = this.props;
     return (
-      <View>
-        <Title>{t("Shop")}</Title>
+      <View style={[styles.container]}>
+        <View style={[styles.wrapperTitle]}>
+          <Title flexWrap="wrap">{t("Shop")}</Title>
+          <View style={[styles.iconWrap]}>
+            <Cart />
+          </View>
+        </View>
         <StyledText.Light>{t("Long Description")}</StyledText.Light>
+        <ItemsList list={list} onPress={id => navigate("product", { id })} />
       </View>
     );
   }
 }
+
+const ItemsList = ({ list, onPress }) => (
+  <FlatList
+    data={list}
+    numColumns={2}
+    columnWrapperStyle={{
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: "space-around"
+    }}
+    keyExtractor={({ id }) => id}
+    renderItem={({ item: { img, title, priceArr, id } }) => (
+      <TouchableOpacity key={id} onPress={() => onPress(id)}>
+        <View style={[styles.itemWrapper]}>
+          <View style={[styles.imgWrapper]}>
+            <Foto
+              position="absolute"
+              left={0}
+              right={0}
+              bottom={0}
+              top={0}
+              borderRadius={8}
+              src={img}
+            />
+          </View>
+          <StyledText.Bold
+            fontSize={12}
+            lineHeight={12}
+            width={163}
+            textAlign="center"
+            containerProps={{
+              paddingBottom: 0,
+              paddingLeft: 0,
+              paddingRight: 0,
+              paddingTop: 10
+            }}
+          >
+            {title}
+          </StyledText.Bold>
+          <StyledText.Bold
+            fontSize={12}
+            width={163}
+            color="#4a4a4a"
+            textAlign="center"
+            containerProps={{
+              paddingBottom: 20,
+              paddingLeft: 0,
+              paddingRight: 0,
+              paddingTop: 0
+            }}
+          >
+            {priceArr.newPrice}
+          </StyledText.Bold>
+        </View>
+      </TouchableOpacity>
+    )}
+  />
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  wrapperTitle: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  iconWrap: {
+    paddingRight: 20,
+    paddingTop: 20,
+    paddingBottom: 20
+  },
+  imgWrapper: {
+    width: 163,
+    height: 163,
+    borderRadius: 8,
+    // borderColor: "#ddd",
+    borderWidth: 0,
+    shadowColor: "#4a4a4a",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1
+  }
+});
 
 export default ShopScreen;
--- stores/index.js
+++ stores/index.js
@@ -5,6 +5,7 @@ import Regions from "./regions";
 import Team from "./team";
 import Video from "./video";
 import Localization from "./localization";
+import Shop from "./shop";
 
 const stores = {};
 stores.navigation = new Navigation(stores);
@@ -13,5 +14,6 @@ stores.regions = new Regions(stores);
 stores.team = new Team(stores);
 stores.video = new Video(stores);
 stores.localization = new Localization(stores);
+stores.shop = new Shop(stores);
 
 export default stores;
