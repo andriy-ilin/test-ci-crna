9f0c2d7564af2c285ad4d4f858aab64a726c020c
andriy-ilin <andriy.ilin90@gmail.com>
2019-02-11 18:53:55 +0200

    feat: find and filter articles on regionsScreen

--- screens/RegionsScreen.js
+++ screens/RegionsScreen.js
@@ -7,15 +7,21 @@ import {
   Text,
   StyleSheet,
   TouchableOpacity,
-  FlatList
+  FlatList,
+  TextInput
 } from "react-native";
 
 import Title from "../components/Title";
 import Foto from "../components/Foto";
+import Line from "../components/Line";
+import Button from "../components/Button";
 import StyledText from "../components/StyledText";
 import Filter from "../icons/Filter";
 import FindBlack from "../icons/FindBlack";
 import MarkerMap from "../icons/MarkerMap";
+import CloseIcon from "../icons/CloseIcon";
+import SelectedIcon from "../icons/SelectedIcon";
+import PlaceholderSearchIcon from "../icons/PlaceholderSearchIcon";
 import Loading from "../components/Loading";
 
 @withNamespaces(["regions"], { wait: true })
@@ -30,6 +36,7 @@ export class RegionsScreen extends Component {
   async componentDidMount() {
     const { regions, lng } = this.props;
     await regions.getRegions(`/catalog/${lng}`);
+    await regions.getRegionsName();
   }
 
   async componentDidUpdate({ lng }) {
@@ -44,9 +51,10 @@ export class RegionsScreen extends Component {
     const {
       t,
       regions,
-      navigation: { navigate }
+      navigation: { navigate },
+      lng
     } = this.props;
-    const { openTab } = this.state;
+    const { openTab, filterTab } = this.state;
 
     return (
       <View style={[styles.wrapper]}>
@@ -65,47 +73,226 @@ export class RegionsScreen extends Component {
           </TouchableOpacity>
         </View>
         <ScrollView>
-          <View style={[styles.filterIconWrapper]}>
-            <TouchableOpacity
-              onPress={() => this.setState({ filterTab: "filter" })}
-            >
-              <View style={{ paddingRight: 20 }}>
-                <Filter />
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => this.setState({ filterTab: "find" })}
-            >
-              <FindBlack />
-            </TouchableOpacity>
+          <View style={[styles.filterModeHeader]}>
+            <View style={[styles.filterIconWrapper]}>
+              <TouchableOpacity
+                onPress={() =>
+                  this.setState({
+                    filterTab: filterTab
+                      ? filterTab !== "find"
+                        ? null
+                        : "filter"
+                      : "filter"
+                  })
+                }
+              >
+                <View style={{ paddingRight: 20 }}>
+                  <Filter />
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() =>
+                  this.setState({
+                    filterTab: filterTab
+                      ? filterTab !== "filter"
+                        ? null
+                        : "find"
+                      : "find"
+                  })
+                }
+              >
+                <FindBlack />
+              </TouchableOpacity>
+            </View>
+            {(regions.selectedRegionsList.length > 0 ||
+              !!regions.findArticlesList) && (
+              <TouchableOpacity
+                onPress={() => {
+                  regions.clearSelectedRegionsList();
+                  regions.clearFindArticlesList();
+                  this.setState({ filterTab: null });
+                }}
+              >
+                <View style={[styles.closeFilterModalWrapper]}>
+                  <CloseIcon width={12} height={11} />
+                  <StyledText.Medium
+                    fontSize={12}
+                    lineHeight={12}
+                    containerProps={{ paddingBottom: 0, paddingLeft: 5 }}
+                  >
+                    clear filter
+                  </StyledText.Medium>
+                </View>
+              </TouchableOpacity>
+            )}
           </View>
 
-          <View style={[openTab !== "regions" && { display: "none" }]}>
-            <RegionsContainer
-              list={regions.listRegions}
-              onPress={region => navigate("regionArticles", { region })}
+          <View style={[filterTab && { display: "none" }]}>
+            <View style={[openTab !== "regions" && { display: "none" }]}>
+              <RegionsContainer
+                list={regions.listRegions}
+                onPress={region => navigate("regionArticles", { region })}
+              />
+            </View>
+
+            <View style={[openTab === "regions" && { display: "none" }]}>
+              <AllStoriesContainer
+                list={regions.listWithFilterAndFind}
+                onPress={id => {
+                  navigate("regionArticleId", { id });
+                }}
+              />
+            </View>
+          </View>
+          {filterTab === "filter" && (
+            <FilterView
+              regions={regions.listRegions || regions.regionsListName}
+              lang={lng}
+              onPress={name => regions.addToSelectedRegions(name)}
+              selectedRegions={regions.selectedRegionsList}
+              onFilter={() =>
+                this.setState({ filterTab: null, openTab: "all" })
+              }
             />
-          </View>
-
-          <View style={[openTab === "regions" && { display: "none" }]}>
-            <AllStoriesContainer
-              list={regions.listEntries}
-              onPress={id => {
-                navigate("regionArticleId", { id });
+          )}
+          {filterTab === "find" && (
+            <FindView
+              value={regions.findArticlesList}
+              onFind={value => {
+                regions.addFindArticles(value);
+                return this.setState({ filterTab: null, openTab: "all" });
               }}
             />
-          </View>
+          )}
         </ScrollView>
-        <TouchableOpacity
-          style={styles.mapButton}
-          onPress={() => this.props.navigation.navigate("map")}
-        >
-          <MarkerMap />
-        </TouchableOpacity>
+        {!filterTab && (
+          <TouchableOpacity
+            style={styles.mapButton}
+            onPress={() => this.props.navigation.navigate("map")}
+          >
+            <MarkerMap />
+          </TouchableOpacity>
+        )}
       </View>
     );
   }
 }
+
+const FilterView = ({
+  regions,
+  lang,
+  onPress = () => {},
+  selectedRegions = [],
+  onFilter = () => {}
+}) => (
+  <View style={[stylesFilterView.wrapper]}>
+    <View style={{ paddingLeft: 20, paddingBottom: 20 }}>
+      <Line />
+    </View>
+    <StyledText.Bold
+      containerProps={{
+        paddingBottom: 10,
+        paddingLeft: 25
+      }}
+    >
+      Regions
+    </StyledText.Bold>
+    {regions.map(({ name }) => (
+      <TouchableOpacity key={name} onPress={() => onPress(name)}>
+        <View key={name} style={[stylesFilterView.regionsFilterItem]}>
+          <View style={[stylesFilterView.selectRow]}>
+            <StyledText.Medium
+              fontSize={14}
+              textTransform="capitalize"
+              containerProps={{
+                paddingBottom: 10,
+                paddingTop: 10,
+                paddingLeft: 25
+              }}
+            >
+              {name}
+            </StyledText.Medium>
+
+            {selectedRegions.includes(name) && <SelectedIcon />}
+          </View>
+          <Line backgroundColor="#eee" />
+        </View>
+      </TouchableOpacity>
+    ))}
+    <View style={{ paddingTop: 20, paddingBottom: 20 }}>
+      <Button onPress={() => onFilter()}>Apply filter</Button>
+    </View>
+  </View>
+);
+
+const stylesFilterView = StyleSheet.create({
+  wrapper: {},
+  regionsFilterItem: {},
+  selectRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingRight: 20
+  }
+});
+
+class FindView extends Component {
+  state = {};
+
+  render() {
+    const { value, onFind } = this.props;
+    return (
+      <View style={[stylesFilterView.wrapper]}>
+        <View style={{ paddingLeft: 20, paddingBottom: 20 }}>
+          <Line />
+        </View>
+        <StyledText.Bold
+          containerProps={{
+            paddingBottom: 10,
+            paddingLeft: 25
+          }}
+        >
+          Search
+        </StyledText.Bold>
+        <View>
+          <View
+            style={{
+              position: "absolute",
+              left: 42,
+              top: 13
+            }}
+          >
+            <PlaceholderSearchIcon />
+          </View>
+          <TextInput
+            style={[stylesFindView.input]}
+            onChangeText={find => this.setState({ find })}
+            defaultValue={value}
+            value={this.state.find}
+            underlineColorAndroid="transparent"
+            placeholder={"Start typing ..."}
+          />
+        </View>
+        <View style={{ paddingTop: 20, paddingBottom: 20 }}>
+          <Button onPress={() => onFind(this.state.find)}>Find articles</Button>
+        </View>
+      </View>
+    );
+  }
+}
+
+const stylesFindView = StyleSheet.create({
+  wrapper: {},
+  input: {
+    height: 40,
+    borderColor: "#cdd3de",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingLeft: 38,
+    marginLeft: 25,
+    marginRight: 25
+  }
+});
 
 const RegionsContainer = ({ list = [], onPress = () => {} }) => (
   <FlatList
@@ -173,13 +360,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginRight: 20
   },
-  filterIconWrapper: {
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    marginLeft: 20,
-    marginBottom: 20
-  },
   cardRegions: {
     justifyContent: "flex-end",
     backgroundColor: "#fff",
@@ -222,6 +402,22 @@ const styles = StyleSheet.create({
     right: 30,
     alignItems: "center",
     justifyContent: "center"
+  },
+  filterModeHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20
+  },
+  filterIconWrapper: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    marginLeft: 20
+  },
+  closeFilterModalWrapper: {
+    flexDirection: "row",
+    alignItems: "center"
   }
 });
 
